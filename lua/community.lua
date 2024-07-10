d0976b80718f7ac0315ac794af42c71fdab5327b
Xinyuan Gu <2793392818@qq.com>
2024-07-10 19:00:07 +0800

    update imported community plugins

--- lua/community.lua
+++ lua/community.lua
@@ -1,12 +1,76 @@
-if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
 
 ---@type LazySpec
+local pack = {
+  { import = "astrocommunity.pack.astro" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.cmake" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.java" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.kotlin" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.python-ruff" },
+  { import = "astrocommunity.pack.rust" },
+  { import = "astrocommunity.pack.toml" },
+  { import = "astrocommunity.pack.yaml" },
+}
+
+---@type LazySpec
+local markdown_and_latex ={
+  { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
+}
+
+---@type LazySpec
+local note_taking = {
+  { import = "astrocommunity.note-taking.neorg" },
+}
+
+---@type LazySpec
+local colorscheme = {
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  { import = "astrocommunity.colorscheme.tokyonight-nvim" },
+  { import = "astrocommunity.colorscheme.nightfox-nvim" },
+  { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
+}
+
+---@type LazySpec
+local editing_support = {
+  -- a better annotation generator
+  { import = "astrocommunity.editing-support.neogen" },
+  -- describe the regex under the cursor, toggled with `gR`
+  { import = "astrocommunity.editing-support.nvim-regexplainer" },
+  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
+  -- provide `:SudaWrite`
+  { import = "astrocommunity.editing-support.suda-vim" },
+  -- <CR> : incremental selection
+  -- <BS> : decremental selection
+  { import = "astrocommunity.editing-support.wildfire-nvim" },
+}
+
+---@type LazySpec
+local indent = {
+  { import = "astrocommunity.indent.indent-rainbowline" },
+}
+
+---@type LazySpec
+local keybinding = {
+  -- { import = "astrocommunity.keybinding.nvcheatsheet-nvim" }, 
+}
+
+---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+  unpack(pack),
+  unpack(markdown_and_latex),
+  unpack(note_taking),
+  unpack(colorscheme),
+  unpack(editing_support),
+  unpack(indent),
+  unpack(keybinding)
 }
--- lua/polish.lua
+++ lua/polish.lua
@@ -1,18 +1,34 @@
-if true then return end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- This will run last in the setup process and is a good place to configure
 -- things like custom filetypes. This just pure lua so anything that doesn't
 -- fit in the normal config locations above can go here
 
 -- Set up custom filetypes
-vim.filetype.add {
-  extension = {
-    foo = "fooscript",
-  },
-  filename = {
-    ["Foofile"] = "fooscript",
-  },
-  pattern = {
-    ["~/%.config/foo/.*"] = "fooscript",
-  },
-}
+-- vim.filetype.add {
+--   extension = {
+--     foo = "fooscript",
+--   },
+--   filename = {
+--     ["Foofile"] = "fooscript",
+--   },
+--   pattern = {
+--     ["~/%.config/foo/.*"] = "fooscript",
+--   },
+-- }
+
+vim.opt.shell = "fish"
+
+if vim.g.neovide then
+  vim.g.neovide_refresh_rate = 144
+  vim.g.neovide_cursor_antialiasing = true
+  vim.g.neovide_cursor_vfx_mode = "railgun"
+
+  vim.g.neovide_transparency = 0.95
+
+  vim.keymap.set("v", "<C-S-c>", '"+y') -- Copy in visual mode
+  vim.keymap.set("n", "<C-S-v>", '"+P') -- Paste in normal mode
+  vim.keymap.set("v", "<C-S-v>", '"+P') -- Paste in visual mode
+  vim.keymap.set("c", "<C-S-v>", "<C-R>+") -- Paste in command mode
+  vim.keymap.set("i", "<C-S-v>", "<C-R>+") -- Paste in insert mode
+  vim.opt.guifont = "FiraCode:h14"
+  vim.opt.linespace = 0
+end
